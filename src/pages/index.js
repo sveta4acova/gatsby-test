f181c63d319dd788e0922277c6ae6aa58711f5c6
sveta <svetlana.shurla4acova@gmail.com>
2020-01-28 10:49:04 +0300

    created test project on Gatsby.js

---
+++ src/pages/index.js
@@ -0,0 +1,58 @@
+import React from "react"
+import { graphql, Link } from "gatsby"
+import styled from "styled-components"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: blue;
+`
+
+const IndexPage = ({ data }) => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div>
+        <h4>{data.allMarkdownRemark.totalCount}</h4>
+        {data.allMarkdownRemark.nodes.map(node => (
+          <div key={node.id}>
+            <BlogLink to={node.fields.slug}>
+              <BlogTitle>
+                {node.frontmatter.title} - {node.frontmatter.date}
+              </BlogTitle>
+            </BlogLink>
+            <p>{node.excerpt}</p>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  )
+}
+
+export default IndexPage
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      nodes {
+        id
+        html
+        frontmatter {
+          date
+          description
+          title
+        }
+        fields {
+          slug
+        }
+        excerpt
+      }
+    }
+  }
+`
